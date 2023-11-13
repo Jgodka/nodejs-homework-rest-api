5b7f548f871c1a57cfef1b6f83d6b21de67d6466
Jgodka <jdzinska77@gmail.com>
2023-11-13 09:58:15 +0200

    Homework

---
+++ controllers/contacts-controller.js
@@ -0,0 +1,82 @@
+import contactService from "../models/contacts.js";
+import { HttpError } from "../helpers/index.js";
+import {
+  contactAddSchema,
+  contactUpdateSchema,
+} from "../schemas/contacts-schemas.js";
+
+const getAllContacts = async (req, res, next) => {
+  try {
+    const result = await contactService.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getContactById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contactService.getById(id);
+    if (!result) {
+      throw HttpError(404, `Not found`);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const add = async (req, res, next) => {
+  try {
+    const { error } = contactAddSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const result = await contactService.addContact(req.body);
+
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contactService.removeContact(id);
+
+    if (!result) {
+      throw HttpError(404, `Not found`);
+    }
+    res.status(200).json({ message: "contact deleted" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateById = async (req, res, next) => {
+  try {
+    const { error } = contactUpdateSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const { id } = req.params;
+    const result = await contactService.updateContact(id, req.body);
+    if (!result) {
+      throw HttpError(404, `Not found`);
+    }
+
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default {
+  getAllContacts,
+  getContactById,
+  add,
+  deleteById,
+  updateById,
+};
